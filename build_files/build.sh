13e67df630f8bed4c2e1ec678c27816051c4f51d
Hunter Frisk <hfrisk+githubpublic@protonmail.com>
2026-09-14 12:21:21 -0500

    build.sh now installs noctalia stack
    - idgaf

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,6 +22,20 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf5 install -y --npgpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+
+#setup noctalia stack
+dnf5 install -y noctalia
+dnf5 install -y noctalia-greeter
+dnf5 install -y umbriel-nightly
+
+# sed -i "s/autostart = \[\]/autostart = ['noctalia']/1" /usr/share/umbriel/config.toml
+# sed -i "s/command =.*/command = \"/usr/bin/noctalia-greeter-session\"" /etc/greetd/config.toml
+
+#replace plasmalogin with greetd
+systemctl disable --now plasmalogin.service
+systemctl enable --now greetd
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
